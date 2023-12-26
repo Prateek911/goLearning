d8f4882ca2247624deeac94fc3d1adb18cf2ca63
pratek911 <prateek.work911@gmail.com>
2023-12-26 11:23:44 +0530

    first commit

---
+++ functions_1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func add(a, b int) int {
+	return a + b
+}
+
+func diff(a, b int) int {
+	return a - b
+}
+
+func main() {
+	resultSum := add(5, 4)
+	fmt.Println("Sum:", resultSum)
+	resultDiff := diff(5, 4)
+	fmt.Println("Diff:", resultDiff)
+}
