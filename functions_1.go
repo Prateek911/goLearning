b31907e69c0dff947b47b3d369c39bd138e49720
pratek911 <prateek.work911@gmail.com>
2023-12-26 11:28:19 +0530

    added multiply function

--- functions_1.go
+++ functions_1.go
@@ -10,9 +10,15 @@ func diff(a, b int) int {
 	return a - b
 }
 
+func multiply(a, b int) int {
+	return a * b
+}
+
 func main() {
 	resultSum := add(5, 4)
 	fmt.Println("Sum:", resultSum)
 	resultDiff := diff(5, 4)
 	fmt.Println("Diff:", resultDiff)
+	resultProduct := multiply(5, 4)
+	fmt.Println("Diff:", resultProduct)
 }
